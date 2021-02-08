f189ef483d4ce8e1fe7f795bf84dd8c6361f9590
Raff <rafal.lazicki99@gmail.com>
2021-02-08 14:58:50 +0100

    fix video capture

--- Mike/face/video.py
+++ Mike/face/video.py
@@ -1,15 +1,15 @@
 import cv2
 
 
-def capture(max_frames=int('inf')):
+def capture(max_frames=float('inf')):
     video = cv2.VideoCapture(1)
     frame = 0
     image = None
     try:
         while cv2.waitKey(1) & 0xFF != ord('q') and (frame := frame+1) < max_frames:
-            image and cv2.imshow('frame', image)
-            _, image = video.capture()
-            if image:
+            image is not None and cv2.imshow('frame', image)
+            _, image = video.read()
+            if len(image):
                 yield image
     finally:
         video.release()
